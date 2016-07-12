15b6159c24d2345869a187f5736619e2de3c07d2
AlbertWang416 <570901294@qq.com>
2016-07-12 10:03:50 +0800

    Add files via upload
    This is all work

---
+++ parcSourceTree/parcSourceTree/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  parcSourceTree
+//
+//  Created by WangQiZheng on 16/7/12.
+//  Copyright © 2016年 WangQiZheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor=[UIColor brownColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
